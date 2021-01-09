ad7712805704e9953cbc3d06b2ce1c42ab1242c7
pranav2010yeet <73528499+pranav2010yeet@users.noreply.github.com>
2021-01-09 10:51:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 function preload(){
-
+    
 }
 
 function setup(){
@@ -15,10 +15,16 @@ color=""
 function draw(){
 image(video,0,0,500,500)
 
+rect(30, 20, 55, 55);
+rect(4, 80, 55, 55, 20, 15, 10, 5);
+rect(90, 80, 55, 55, 20, 15, 10, 5);
+
 tint(color)
 }
 
 function take_snapshot(){
     save("pranav.png")
 }
+
+circle(30, 30, 20);
 
